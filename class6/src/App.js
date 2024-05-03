86f4fd334e73d88ef1a7c057d6bd76a5ea36df83
petitmatinz <162280079+petitmatinz@users.noreply.github.com>
2024-05-03 19:59:55 +0900

    test

--- class6/src/App.js
+++ class6/src/App.js
@@ -6,7 +6,7 @@ function App() {
       <div>대충 부재목이라는 뜻</div>
       <Card subj="안녕하세요1" desc="반갑습니다"></Card>
       <Card subj="안녕하세요2" desc="반갑습니다"></Card>
-      <Card subj="안녕하세요3" desc="반갑습니다"></Card>
+      <Card subj="안녕하세요3" desc="반갑습니다!!!"></Card>
     </div>
   );
 }
